06cec71596ea2d04021d55a480d6388ffd8440b2
WojTechM <wojciech.m1997@gmail.com>
2019-04-24 19:29:53 +0200

    Dodano testy do algorytmów przeszukiwania w głąb i wszerz

---
+++ src/test/java/com/github/wojtechm/drzewa/TestyWszerz.java
@@ -0,0 +1,31 @@
+package com.github.wojtechm.drzewa;
+
+import org.testng.annotations.Test;
+
+/**
+ * @author Makiela Wojciech
+ */
+class TestyWszerz {
+
+    @Test
+    public void czyDziałaNaSymetrycznymDrzewie() {
+        // arrange
+        PrzeszukiwanieWszerz wszerz = new PrzeszukiwanieWszerz();
+        String oczekiwane = "[5, 3, 7, 2, 4, 6, 8]";
+        // act
+        String coDostałem = wszerz.czytajDrzewo(DrzewaDoTestowania.symetryczne);
+        // assert
+        assert oczekiwane.equals(coDostałem) : String.format("Expected %s, got %s", oczekiwane, coDostałem);
+    }
+
+    @Test
+    public void czyDziałaNaNiesymetrycznymDrzewie() {
+        // arrange
+        PrzeszukiwanieWszerz wszerz = new PrzeszukiwanieWszerz();
+        String oczekiwane = "[0, 2, 1, 4, 3, 6, 5]";
+        // act
+        String coDostałem = wszerz.czytajDrzewo(DrzewaDoTestowania.niesymetryczne);
+        // assert
+        assert oczekiwane.equals(coDostałem) : String.format("Expected %s, got %s", oczekiwane, coDostałem);
+    }
+}
