ff0e4a1cd1eb2dd939f1ceed152d7d21aff35cf4
WojTechM <wojciech.m1997@gmail.com>
2019-04-24 19:29:53 +0200

    Dodano testy do algorytmów przeszukiwania w głąb i wszerz

---
+++ src/test/java/com/github/wojtechm/drzewa/TestyWGłąb.java
@@ -0,0 +1,32 @@
+package com.github.wojtechm.drzewa;
+
+import org.testng.annotations.Test;
+
+/**
+ * @author Makiela Wojciech
+ */
+@Test
+class TestyWGłąb {
+
+    @Test
+    public void czyDziałaNaSymetrycznymDrzewie() {
+        // arrange
+        PrzeszukiwanieWGłąb wGłąb = new PrzeszukiwanieWGłąb();
+        String oczekiwane = "[5, 3, 2, 4, 7, 6, 8]";
+        // act
+        String coDostałem = wGłąb.czytajDrzewo(DrzewaDoTestowania.symetryczne);
+        // assert
+        assert oczekiwane.equals(coDostałem) : String.format("Expected %s, got %s", oczekiwane, coDostałem);
+    }
+
+    @Test
+    public void czyDziałaNaNiesymetrycznymDrzewie(){
+        // arrange
+        PrzeszukiwanieWGłąb wGłąb = new PrzeszukiwanieWGłąb();
+        String oczekiwane = "[0, 2, 1, 4, 3, 6, 5]";
+        // act
+        String coDostałem = wGłąb.czytajDrzewo(DrzewaDoTestowania.niesymetryczne);
+        // assert
+        assert oczekiwane.equals(coDostałem) : String.format("Expected %s, got %s", oczekiwane, coDostałem);
+    }
+}
