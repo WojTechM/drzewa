e657c3683b3f994d326c4e226689625c31075e64
WojTechM <wojciech.m1997@gmail.com>
2019-04-25 18:00:45 +0200

    Dodano puste klasy do zaimplementowania do pakietu z zadaniem drugim

---
+++ src/main/java/com/github/wojtechm/drzewa/zadanie_drugie/PrzeszukiwanieWGłąb.java
@@ -0,0 +1,10 @@
+package com.github.wojtechm.drzewa.zadanie_drugie;
+
+/**
+ * @author Makiela Wojciech
+ */
+class PrzeszukiwanieWGłąb {
+    String czytajGraf(WęzełGrafu korzeń) {
+        return null;
+    }
+}
---
+++ src/main/java/com/github/wojtechm/drzewa/zadanie_drugie/PrzeszukiwanieWszerz.java
@@ -0,0 +1,10 @@
+package com.github.wojtechm.drzewa.zadanie_drugie;
+
+/**
+ * @author Makiela Wojciech
+ */
+class PrzeszukiwanieWszerz {
+    String czytajGraf(WęzełGrafu korzeń) {
+        return null;
+    }
+}
